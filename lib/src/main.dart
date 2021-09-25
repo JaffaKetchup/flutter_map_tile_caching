63ec384362f06dc117fd4ea9e323c72f8a17bd5f
Abdelrahman Sherif <abderahmansherif@gmail.com>
2021-09-25 21:32:48 +0200

    Update main.dart

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -581,7 +581,7 @@ class StorageCachingTileProvider extends TileProvider {
       ).then((value) {
         try {
           successfulTiles += value[0] as int;
-          failedTiles.addAll(value[1]);
+          failedTiles.add(value[1]);
           seaTiles += value[2] as int;
           existingTiles += value[3] as int;
         } catch (e) {}
