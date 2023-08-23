eaa0fc3d4a65943352dedbe8717fb8b7066e188a
JaffaKetchup <github@jaffaketchup.dev>
2023-08-23 09:44:58 +0100

    Increased timeouts on tests
    Former-commit-id: 3342095157985e80bef1687dc8016430b28a7743 [formerly 4febb6dcc04d2dae739ee0135ebb743c02a05b48]
    Former-commit-id: 08b8f1bffe1a546057725e5c3bd783aea61e54e6

--- test/fmtc_test.dart
+++ test/fmtc_test.dart
@@ -43,197 +43,218 @@ void main() {
     return evts;
   }
 
-  group('Rectangle Region', () {
-    final rectRegion =
-        RectangleRegion(LatLngBounds(const LatLng(-1, -1), const LatLng(1, 1)))
-            .toDownloadable(minZoom: 1, maxZoom: 16, options: TileLayer());
+  group(
+    'Rectangle Region',
+    () {
+      final rectRegion = RectangleRegion(
+        LatLngBounds(const LatLng(-1, -1), const LatLng(1, 1)),
+      ).toDownloadable(minZoom: 1, maxZoom: 16, options: TileLayer());
 
-    test(
-      'Count By Counter',
-      () => expect(TilesCounter.rectangleTiles(rectRegion), 179196),
-    );
+      test(
+        'Count By Counter',
+        () => expect(TilesCounter.rectangleTiles(rectRegion), 179196),
+      );
 
-    test(
-      'Count By Generator',
-      () async => expect(await countByGenerator(rectRegion), 179196),
-    );
+      test(
+        'Count By Generator',
+        () async => expect(await countByGenerator(rectRegion), 179196),
+      );
 
-    test(
-      'Counter Duration',
-      () => print(
-        '${List.generate(
-          2000,
-          (index) {
-            final clock = Stopwatch()..start();
-            TilesCounter.rectangleTiles(rectRegion);
-            clock.stop();
-            return clock.elapsedMilliseconds;
-          },
-          growable: false,
-        ).average} ms',
-      ),
-    );
+      test(
+        'Counter Duration',
+        () => print(
+          '${List.generate(
+            2000,
+            (index) {
+              final clock = Stopwatch()..start();
+              TilesCounter.rectangleTiles(rectRegion);
+              clock.stop();
+              return clock.elapsedMilliseconds;
+            },
+            growable: false,
+          ).average} ms',
+        ),
+      );
 
-    test(
-      'Generator Duration',
-      () async {
-        final clock = Stopwatch()..start();
-        await countByGenerator(rectRegion);
-        clock.stop();
-        print('${clock.elapsedMilliseconds / 1000} s');
-      },
-    );
-  });
+      test(
+        'Generator Duration',
+        () async {
+          final clock = Stopwatch()..start();
+          await countByGenerator(rectRegion);
+          clock.stop();
+          print('${clock.elapsedMilliseconds / 1000} s');
+        },
+      );
+    },
+    timeout: const Timeout(Duration(minutes: 1)),
+  );
 
-  group('Circle Region', () {
-    final circleRegion = CircleRegion(const LatLng(0, 0), 200)
-        .toDownloadable(minZoom: 1, maxZoom: 15, options: TileLayer());
+  group(
+    'Circle Region',
+    () {
+      final circleRegion = CircleRegion(const LatLng(0, 0), 200)
+          .toDownloadable(minZoom: 1, maxZoom: 15, options: TileLayer());
 
-    test(
-      'Count By Counter',
-      () => expect(TilesCounter.circleTiles(circleRegion), 61564),
-    );
+      test(
+        'Count By Counter',
+        () => expect(TilesCounter.circleTiles(circleRegion), 61564),
+      );
 
-    test(
-      'Count By Generator',
-      () async => expect(await countByGenerator(circleRegion), 61564),
-    );
+      test(
+        'Count By Generator',
+        () async => expect(await countByGenerator(circleRegion), 61564),
+      );
 
-    test(
-      'Counter Duration',
-      () => print(
-        '${List.generate(
-          500,
-          (index) {
-            final clock = Stopwatch()..start();
-            TilesCounter.circleTiles(circleRegion);
-            clock.stop();
-            return clock.elapsedMilliseconds;
-          },
-          growable: false,
-        ).average} ms',
-      ),
-    );
+      test(
+        'Counter Duration',
+        () => print(
+          '${List.generate(
+            500,
+            (index) {
+              final clock = Stopwatch()..start();
+              TilesCounter.circleTiles(circleRegion);
+              clock.stop();
+              return clock.elapsedMilliseconds;
+            },
+            growable: false,
+          ).average} ms',
+        ),
+      );
 
-    test(
-      'Generator Duration',
-      () async {
-        final clock = Stopwatch()..start();
-        await countByGenerator(circleRegion);
-        clock.stop();
-        print('${clock.elapsedMilliseconds / 1000} s');
-      },
-    );
-  });
+      test(
+        'Generator Duration',
+        () async {
+          final clock = Stopwatch()..start();
+          await countByGenerator(circleRegion);
+          clock.stop();
+          print('${clock.elapsedMilliseconds / 1000} s');
+        },
+      );
+    },
+    timeout: const Timeout(Duration(minutes: 1)),
+  );
 
-  group('Line Region', () {
-    final lineRegion = LineRegion(
-      [const LatLng(-1, -1), const LatLng(1, 1), const LatLng(1, -1)],
-      5000,
-    ).toDownloadable(minZoom: 1, maxZoom: 15, options: TileLayer());
+  group(
+    'Line Region',
+    () {
+      final lineRegion = LineRegion(
+        [const LatLng(-1, -1), const LatLng(1, 1), const LatLng(1, -1)],
+        5000,
+      ).toDownloadable(minZoom: 1, maxZoom: 15, options: TileLayer());
 
-    test(
-      'Count By Counter',
-      () => expect(TilesCounter.lineTiles(lineRegion), 5040),
-    );
+      test(
+        'Count By Counter',
+        () => expect(TilesCounter.lineTiles(lineRegion), 5040),
+      );
 
-    test(
-      'Count By Generator',
-      () async => expect(await countByGenerator(lineRegion), 5040),
-    );
+      test(
+        'Count By Generator',
+        () async => expect(await countByGenerator(lineRegion), 5040),
+      );
 
-    test(
-      'Counter Duration',
-      () => print(
-        '${List.generate(
-          300,
-          (index) {
-            final clock = Stopwatch()..start();
-            TilesCounter.lineTiles(lineRegion);
-            clock.stop();
-            return clock.elapsedMilliseconds;
-          },
-          growable: false,
-        ).average} ms',
-      ),
-    );
+      test(
+        'Counter Duration',
+        () => print(
+          '${List.generate(
+            300,
+            (index) {
+              final clock = Stopwatch()..start();
+              TilesCounter.lineTiles(lineRegion);
+              clock.stop();
+              return clock.elapsedMilliseconds;
+            },
+            growable: false,
+          ).average} ms',
+        ),
+      );
 
-    test(
-      'Generator Duration',
-      () async {
-        final clock = Stopwatch()..start();
-        await countByGenerator(lineRegion);
-        clock.stop();
-        print('${clock.elapsedMilliseconds / 1000} s');
-      },
-    );
-  });
+      test(
+        'Generator Duration',
+        () async {
+          final clock = Stopwatch()..start();
+          await countByGenerator(lineRegion);
+          clock.stop();
+          print('${clock.elapsedMilliseconds / 1000} s');
+        },
+      );
+    },
+    timeout: const Timeout(Duration(minutes: 1)),
+  );
 
-  group('Custom Polygon Region', () {
-    final customPolygonRegion1 = CustomPolygonRegion([
-      const LatLng(51.45818683312154, -0.9674646220840917),
-      const LatLng(51.55859639937614, -0.9185366064186982),
-      const LatLng(51.476641197796724, -0.7494743298246318),
-      const LatLng(51.56029831737391, -0.5322770067805148),
-      const LatLng(51.235701626195365, -0.5746290119276093),
-      const LatLng(51.38781341753136, -0.6779891095601829),
-    ]).toDownloadable(minZoom: 1, maxZoom: 17, options: TileLayer());
+  group(
+    'Custom Polygon Region',
+    () {
+      final customPolygonRegion1 = CustomPolygonRegion([
+        const LatLng(51.45818683312154, -0.9674646220840917),
+        const LatLng(51.55859639937614, -0.9185366064186982),
+        const LatLng(51.476641197796724, -0.7494743298246318),
+        const LatLng(51.56029831737391, -0.5322770067805148),
+        const LatLng(51.235701626195365, -0.5746290119276093),
+        const LatLng(51.38781341753136, -0.6779891095601829),
+      ]).toDownloadable(minZoom: 1, maxZoom: 17, options: TileLayer());
 
-    final customPolygonRegion2 = CustomPolygonRegion([
-      const LatLng(-1, -1),
-      const LatLng(1, -1),
-      const LatLng(1, 1),
-      const LatLng(-1, 1),
-    ]).toDownloadable(minZoom: 1, maxZoom: 17, options: TileLayer());
+      final customPolygonRegion2 = CustomPolygonRegion([
+        const LatLng(-1, -1),
+        const LatLng(1, -1),
+        const LatLng(1, 1),
+        const LatLng(-1, 1),
+      ]).toDownloadable(minZoom: 1, maxZoom: 17, options: TileLayer());
 
-    test(
-      'Count By Counter',
-      () =>
-          expect(TilesCounter.customPolygonTiles(customPolygonRegion1), 15962),
-    );
+      test(
+        'Count By Counter',
+        () => expect(
+          TilesCounter.customPolygonTiles(customPolygonRegion1),
+          15962,
+        ),
+      );
 
-    test(
-      'Count By Generator',
-      () async => expect(await countByGenerator(customPolygonRegion1), 15962),
-    );
+      test(
+        'Count By Generator',
+        () async => expect(await countByGenerator(customPolygonRegion1), 15962),
+      );
 
-    test(
-      'Count By Counter (Compare to Rectangle Region)',
-      () =>
-          expect(TilesCounter.customPolygonTiles(customPolygonRegion2), 712096),
-    );
+      test(
+        'Count By Counter (Compare to Rectangle Region)',
+        () => expect(
+          TilesCounter.customPolygonTiles(customPolygonRegion2),
+          712096,
+        ),
+      );
 
-    test(
-      'Count By Generator (Compare to Rectangle Region)',
-      () async => expect(await countByGenerator(customPolygonRegion2), 712096),
-    );
+      test(
+        'Count By Generator (Compare to Rectangle Region)',
+        () async =>
+            expect(await countByGenerator(customPolygonRegion2), 712096),
+      );
 
-    test(
-      'Counter Duration',
-      () => print(
-        '${List.generate(
-          500,
-          (index) {
-            final clock = Stopwatch()..start();
-            TilesCounter.customPolygonTiles(customPolygonRegion1);
-            clock.stop();
-            return clock.elapsedMilliseconds;
-          },
-          growable: false,
-        ).average} ms',
-      ),
-    );
+      test(
+        'Counter Duration',
+        () => print(
+          '${List.generate(
+            500,
+            (index) {
+              final clock = Stopwatch()..start();
+              TilesCounter.customPolygonTiles(customPolygonRegion1);
+              clock.stop();
+              return clock.elapsedMilliseconds;
+            },
+            growable: false,
+          ).average} ms',
+        ),
+      );
 
-    test(
-      'Generator Duration',
-      () async {
-        final clock = Stopwatch()..start();
-        await countByGenerator(customPolygonRegion1);
-        clock.stop();
-        print('${clock.elapsedMilliseconds / 1000} s');
-      },
-    );
-  });
+      test(
+        'Generator Duration',
+        () async {
+          final clock = Stopwatch()..start();
+          await countByGenerator(customPolygonRegion1);
+          clock.stop();
+          print('${clock.elapsedMilliseconds / 1000} s');
+        },
+      );
+    },
+    timeout: const Timeout(Duration(minutes: 1)),
+  );
 }
 
 /*
