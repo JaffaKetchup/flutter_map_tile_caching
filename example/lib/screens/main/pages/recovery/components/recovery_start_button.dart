63df69b43fe4f5760c02cffa1911f77f70dfe969
JaffaKetchup <github@jaffaketchup.dev>
2024-02-24 16:45:49 +0000

    Minor fix in example app
    Former-commit-id: 6dc02904a437cbe872174d586fbdfea7af18e3ae [formerly 1397c8d9a6d91fe3e713deaa7f54a7e14b97ca7b]
    Former-commit-id: 91a001d89f173b2872d98df368db8228d3947f87

--- example/lib/screens/main/pages/recovery/components/recovery_start_button.dart
+++ example/lib/screens/main/pages/recovery/components/recovery_start_button.dart
@@ -17,56 +17,38 @@ class RecoveryStartButton extends StatelessWidget {
   final ({bool isFailed, RecoveredRegion region}) result;
 
   @override
-  Widget build(BuildContext context) => FutureBuilder<int>(
-        future: const FMTCStore('')
-            .download
-            .check(result.region.toDownloadable(TileLayer())),
-        builder: (context, tiles) => tiles.hasData
-            ? IconButton(
-                icon: Icon(
-                  Icons.download,
-                  color: result.isFailed ? Colors.green : null,
-                ),
-                onPressed: !result.isFailed
-                    ? null
-                    : () async {
-                        final regionSelectionProvider =
-                            Provider.of<RegionSelectionProvider>(
-                          context,
-                          listen: false,
-                        )
-                              ..region = result.region.toRegion()
-                              ..minZoom = result.region.minZoom
-                              ..maxZoom = result.region.maxZoom
-                              ..setSelectedStore(
-                                FMTCStore(result.region.storeName),
-                              );
-                        // TODO: Check
-                        //..regionTiles = tiles.data;
+  Widget build(BuildContext context) => IconButton(
+        icon: Icon(
+          Icons.download,
+          color: result.isFailed ? Colors.green : null,
+        ),
+        onPressed: !result.isFailed
+            ? null
+            : () async {
+                final regionSelectionProvider =
+                    Provider.of<RegionSelectionProvider>(context, listen: false)
+                      ..region = result.region.toRegion()
+                      ..minZoom = result.region.minZoom
+                      ..maxZoom = result.region.maxZoom
+                      ..setSelectedStore(
+                        FMTCStore(result.region.storeName),
+                      );
 
-                        await Navigator.of(context).push(
-                          MaterialPageRoute<String>(
-                            builder: (context) => ConfigureDownloadPopup(
-                              region: regionSelectionProvider.region!,
-                              minZoom: result.region.minZoom,
-                              maxZoom: result.region.maxZoom,
-                            ),
-                            fullscreenDialog: true,
-                          ),
-                        );
+                // TODO: Check
+                //..regionTiles = tiles.data;
 
-                        moveToDownloadPage();
-                      },
-              )
-            : const Padding(
-                padding: EdgeInsets.all(8),
-                child: SizedBox(
-                  height: 24,
-                  width: 24,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 3,
+                await Navigator.of(context).push(
+                  MaterialPageRoute<String>(
+                    builder: (context) => ConfigureDownloadPopup(
+                      region: regionSelectionProvider.region!,
+                      minZoom: result.region.minZoom,
+                      maxZoom: result.region.maxZoom,
+                    ),
+                    fullscreenDialog: true,
                   ),
-                ),
-              ),
+                );
+
+                moveToDownloadPage();
+              },
       );
 }
