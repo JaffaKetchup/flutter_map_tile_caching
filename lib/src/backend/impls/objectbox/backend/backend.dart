022bbe0d3a4a4949d302f691bb11dcc344120a90
Luka S <github@jaffaketchup.dev>
2025-03-09 18:28:58 +0000

    v10.1.1: fix import/export operations on iOS  (#180)

--- lib/src/backend/impls/objectbox/backend/backend.dart
+++ lib/src/backend/impls/objectbox/backend/backend.dart
@@ -9,9 +9,8 @@ import 'dart:isolate';
 import 'dart:math';
 
 import 'package:collection/collection.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 
