3c065ce1515ff2e23860be5d7440c87292249832
mpeter50 <83356418+mpeter50@users.noreply.github.com>
2026-08-16 20:08:48 +0200

    remove objextbox

--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  objectbox_flutter_libs
   share_plus
   sqlite3_flutter_libs
   url_launcher_windows
--- lib/src/backend/export_external.dart
+++ lib/src/backend/export_external.dart
@@ -4,8 +4,6 @@
 export 'errors/errors.dart';
 export 'impls/drift/web/backend.dart'
     if (dart.library.ffi) 'impls/drift/native/backend/backend.dart';
-export 'impls/objectbox/web_noop/backend.dart'
-    if (dart.library.ffi) 'impls/objectbox/native/backend/backend.dart';
 export 'interfaces/backend/backend.dart';
 export 'interfaces/backend/internal.dart';
 export 'interfaces/backend/internal_thread_safe.dart';
